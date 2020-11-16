21576c00dd4266591037b70c6725572062cc5158
root <root@DESKTOP-Q0RH7LK.localdomain>
2020-11-16 14:36:35 +0300

    update backend

--- app/api/backend/backend.py
+++ app/api/backend/backend.py
@@ -9,10 +9,19 @@ def add_new_user(email, pass_, ph_num, fio, passp):
 
 def add_new_trip(from_, to, depar_date, arrival_date, transport_id, distance, price, name):
     db = pymongo.MongoClient("mongodb://db:27017/").example
-    id = db.ticket.find_one({"name": name}).get('_id')
     db.trip.insert([{"from": from_, "to": to, "depar_date": depar_date, "arrival_date": arrival_date,
-                     "transport_id": transport_id, "ticket_id": id, "distance": distance,
+                     "transport_id": transport_id, "ticket_id": get_ticket_type_id(name), "distance": distance,
                      "price": price}])
+
+
+def find_trip(from_, to, depar_date, name):
+    db = pymongo.MongoClient("mongodb://db:27017/").example
+    return list(db.trip.find({"depar_date": {"$gte": depar_date}, "from": from_, "to": to}))
+
+
+def get_ticket_type_id(name):
+    db = pymongo.MongoClient("mongodb://db:27017/").example
+    return db.ticket.find_one({"name": name}).get('_id')
 
 
 def add_trip_to_user(user_id, trip_id):
