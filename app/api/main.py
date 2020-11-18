4637379b4387c947dce6446035362f3dd2854efa
Eg0R0828 <egorhh357@gmail.com>
2020-11-18 18:08:05 +0300

    frontend started

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,9 @@
 import pymongo
-from backend import backend
+from api.backend import backend
 import datetime
 
 
-backend.add_new_user("ddd","ddd","ddd","ddd","ddd")
+backend.add_new_user("ddd", "ddd", "ddd", "ddd", "ddd")
 backend.add_new_ticket("pes")
 d = datetime.datetime.strptime("2017-10-13T10:53:53.000Z", "%Y-%m-%dT%H:%M:%S.000Z")
 backend.add_new_trip("ddd", "ddd", d, "ddd", "pes", "ddd", "ddd", "pes")
