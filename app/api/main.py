dee9edd683942eccae86f7cc2bb668dcc53af287
Trushnikov Andrey <31585580+AndersonGH@users.noreply.github.com>
2020-12-01 21:50:49 +0300

    Update main.py

--- app/api/main.py
+++ app/api/main.py
@@ -7,8 +7,6 @@ import datetime
 backend.clear_data()
 backend.import_data()
 
-backend.import_kind_of_transport('data/kind_of_transport.json')
-
 db = pymongo.MongoClient("mongodb://db:27017/").example
 print(list(db.kind_of_transport.find({})))
 print(list(db.transport.find({})))
